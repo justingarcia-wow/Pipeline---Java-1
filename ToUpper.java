0bc3d0e995689acc3853c9d559da1e95763af37e
justingarcia-wow <justin40412@gmail.com>
2026-06-08 00:02:21 -0500

    add java files

---
+++ ToUpper.java
@@ -0,0 +1,11 @@
+public class ToUpper {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            String input = args[0];
+            String output = input.toUpperCase();
+            System.out.println(output);
+        } else {
+            System.out.println("No input provided!");
+        }
+    }
+}
